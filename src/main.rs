b4aa2ba8cb22d672f96a4cc7636893c1cbc141b0
Stefan Lang <Stefan.Lang@med.lu.se>
2023-03-22 13:47:16 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+use needletail::parse_fastx_file;
+use kmers::naive_impl::Kmer;
+
+fn main() {
+    let l = 1;
+    
+    println!("u8  {l:#008b}\nu16 {l:#016b}\nu32 {l:#032b}\nu64 {l:#064b}\n");
+}
